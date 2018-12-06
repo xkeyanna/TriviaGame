48a3d637bf086d2308d4dd5f2270092d30b645f9
xkeekyx <xkeyanna@yahoo.com>
2018-12-06 15:29:50 -0800

    updated pages but problem with photos linking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,188 @@
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+/* Pseudo Code 
+ 
+Click to Start
+Timer begins at 60 seconds and countdown
+Player goes through all 10 questions
+player can only guess one answer per question
+Once completed, player submit's answers
+HTML is updated with users score
+Score includes: time spent, answers correct, and answers wrong */
 
-  // Get todays date and time
-  var now = new Date().getTime();
+// --------------------------------------------------------------- 
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+var questions = [{
+	ques: "When should you book your flight tickets for the best price?",
+	ans: ["ASAP", "The day before", "6 months ahead", "2 weeks ahead"],
+	name: "when",
+	correct: "ASAP",
+	divClass: ".when"
+},
+{
+	ques: "How early can you check in for your flight, typically?",
+	ans: ["48 hours", "24 hours", "12 hours", "72 hours"],
+	name: "checkIn",
+	correct: "24 hours",
+	divClass: ".checkIn"
+},
+{
+	ques: "How many lbs is your cabin-pet allowed to weigh?",
+	ans: ["10 lbs", "15 lbs", "20 lbs", "30 lbs"],
+	name: "pounds",
+	correct: "20 lbs",
+	divClass: ".pounds"
+},
+{
+	ques: "When did airplane travel go commercial?",
+	ans: ["1912", "1895", "1942", "1914"],
+	name: "commercial",
+	correct: "1914",
+	divClass: ".commercial"
+},
+{
+	ques: "Who was the first female pilot?",
+	ans: ["Amelia Warner", "Amelia Campbell", "Amelia Earhart", "Amelia Hall"],
+	name: "firstFemale",
+	correct: "Amelia Earhart",
+	divClass: ".firstFemale"
+},
+{
+	ques: "Who created airplanes that could fly?",
+	ans: ["The Wright Brothers", "Brian McKnight", "Tinker Hatfield", "The Right Brothers"],
+	name: "whoCreated",
+	correct: "writeBrothers",
+	divClass: ".whoCreated"
+},
+{
+	ques: "How many continents are there?",
+	ans: ["4", "12", "127", "7"],
+	name: "continents",
+	correct: "7",
+	divClass: ".continents"
+},
+{
+	ques: "Which airline is the largest in the world?",
+	ans: ["Delta Airlines", "American Airlines", "US Airways", "British Airways"],
+	name: "largestAirline",
+	correct: "American Airlines",
+	divClass: ".largestAirline"
+},
+{
+	ques: "When is the cheapest time to travel?",
+	ans: ["Early December", "Mid-June", "Early April", "Late February"],
+	name: "cheapMonth",
+	correct: "Early December",
+	divClass: ".cheapMonth"
+},
+{
+	ques: "Which country has the highest passenger tax?",
+	ans: ["Mexico", "USA", "UK", "China"],
+	name: "tax",
+	correct: "UK",
+	divClass: ".tax"
+}
+] // end questions object
 
-  // Time calculations for days, hours, minutes and seconds
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var labels = ["first", "second", "third", "forth"];
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = seconds + "seconds left! ";
+// click to start then display quesions
+var startGame = $("#start-btn").on('click', function() {
+$(this).parent().hide();
+$('.container').show();
+countdown(60);
+questionDisplay();
+});
 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "OUt of time!";
-  }
+// function for displaying questions
+var questionDisplay = function() {
+$(".questions :not('#sub-but')").empty();
+// loops through the 10 questions 
+for (var j = 0; j < 10; j++) {
+$('.questions').prepend('<div class="' + questions[j].name + '"></div>');
+$(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
+// loops through answers for each radio button
+for (var i = 0; i <= 3; i++) {
+	$(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
+}
+$('.questions').prepend('<hr />');
+}
+}
+
+
+// function for countdown timer
+var countdown = function(seconds) {
+
+var timer = setInterval(function() {
+seconds = seconds - 1;
+$("#time-remain").html(seconds);
+
+if (seconds <= 0) {
+	$('.container').fadeOut(500);
+	var correctAnswers = 0;
+	var wrongAnswers = 0;
+	var unAnswered = 0;
+
+	// loop through correctArray & radioName to match html elements & answers
+	for (var i = 0; i < 10; i++) {
+
+		if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+
+			correctAnswers++;
+			console.log("this is correct! number:" + i)
+		} else {
+			wrongAnswers++;
+			console.log("this is wrong! number:" + i)
+		};
+	}
+	$('#correctTimesUp').append(correctAnswers);
+	// display wrongAnswers
+	$('#wrongTimesUp').append(wrongAnswers);
+	$('#timesUp').fadeIn(1000).show();
+
+	// alert("Times Up!");
+	clearInterval(timer);
+	return;
+}
 }, 1000);
 
-
-function check(){
-
-	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
-	var question3 = document.quiz.question3.value;
-	var correct = 0;
+// click event for submit button to stop timer
+$('#sub-but').on('click', function() {
+clearInterval(timer);
+})
+}; // end countdown
 
 
-	if (question1 == "Providence") {
-		correct++;
-}
-	if (question2 == "Hartford") {
-		correct++;
-}	
-	if (question3 == "Albany") {
-		correct++;
-	}
-	
-	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
-	var messages = ["Great job!", "That's just okay", "You really need to do better"];
-	var score;
+// function to grade quiz once submit button is clicked
+var gradeQuiz = $('#sub-but').on('click', function() {
 
-	if (correct == 0) {
-		score = 2;
-	}
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var unAnswered = 0;
 
-	if (correct > 0 && correct < 3) {
-		score = 1;
-	}
+// loop through correctArray & radioName to match html elements & answers
+for (var i = 0; i < 10; i++) {
 
-	if (correct == 3) {
-		score = 0;
-	}
+if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
 
-	document.getElementById("after_submit").style.visibility = "visible";
+	correctAnswers++;
+} else {
+	wrongAnswers++;
+};
+};
 
-	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-	document.getElementById("picture").src = pictures[score];
-	}
+// once submit is clicked...
+// tests
+// stop timer
+countdown();
+// fade out questions
+$('.container').fadeOut(500);
+// show answerScreen
+$('#answerScreen').show();
+// display correctAnswers
+$('#correctScreen').append(correctAnswers);
+// display wrongAnswers
+$('#wrongScreen').append(wrongAnswers);
+
+}); 
+// end
